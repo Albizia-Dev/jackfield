859564e09d924d2c7f2535b816a6f9766a4e0718
Nikolay Chupin <nickchupin@yandex.com>
2026-09-23 21:45:24 +0700

    fix: align plugin platform minimum versions

--- macos/jackfield/Package.swift
+++ macos/jackfield/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "jackfield",
     platforms: [
-        .macOS("10.15")
+        .macOS("11.0")
     ],
     products: [
         .library(name: "jackfield", targets: ["jackfield"])
